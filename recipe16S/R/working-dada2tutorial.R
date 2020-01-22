bd3ad7d52b5bc5149b00e380793daaca994c8aeb
Emily Bean <ebean@uwyo.edu>
2020-01-22 11:23:51 -0500

    upload dada2 script working

---
+++ recipe16S/R/working-dada2tutorial.R
@@ -0,0 +1,9 @@
+
+## WORKING: dada2
+
+## Install
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  {install.packages("BiocManager")}
+BiocManager::install("dada2", version = "3.10")
+
+# If already installed: 
